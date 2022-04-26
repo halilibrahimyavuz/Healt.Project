e5734577fd827b3ab87f98fb78f20ef43979468d
halil <halilibrahimyavuz07@gmail.com>
2022-04-27 00:52:27 +0300

    US20

--- src/test/java/pages/US20_pages.java
+++ src/test/java/pages/US20_pages.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -10,13 +12,75 @@ public class US20_pages {
 
     //@FindBy notasyonu ile locatelerı buraya koyacagız.
 
+    @FindBy (xpath ="//*[text()='Administration']")
+    public WebElement administrationButonu;
+
+    @FindBy (xpath ="//*[text()='User management']")
+    public WebElement userManagementButonu;
+
+    @FindBy (xpath = "//*[text()='Users']")
+    public WebElement usersPageText;
+
+    @FindBy (xpath = "(//span[@class='d-none d-md-inline'])[1]")
+    public WebElement viewButonu;
+
+    @FindBy (xpath = "//*[text()='First name']")
+    public  WebElement firstnameText;
+
+    @FindBy (xpath = "//*[text()='Last name']")
+    public  WebElement lastnameText;
+
+    @FindBy (xpath = "//*[text()='Email']")
+    public  WebElement emailText;
+
+    @FindBy (xpath = "//*[text()='Profiles']")
+    public  WebElement profilesText;
+
+    @FindBy (xpath = "//*[text()='Admin Halil']")
+    public WebElement adminHalilButonu;
 
 
+    @FindBy (xpath = "//*[text()='Sign out']")
+    public WebElement singOutButonu;
 
+    @FindBy (xpath = "(//*[text()='Edit'])[1]")
+    public WebElement editButonu;
 
+    @FindBy (xpath = "//*[text()='Create or edit a user']")
+    public WebElement createEditUserPagesText;
 
+    @FindBy (xpath = "//*[text()='ROLE_ADMIN']")
+    public WebElement roleAdmin;
 
+    @FindBy (xpath = "//*[text()='ROLE_USER']")
+    public WebElement roleUser;
 
+    @FindBy (xpath = "//*[text()='ROLE_PATIENT']")
+    public WebElement rolePatient;
+
+    @FindBy (xpath = "//*[text()='ROLE_STAFF']")
+    public WebElement roleStaff;
+
+    @FindBy (xpath = "//*[text()='ROLE_PHYSICIAN']")
+    public WebElement rolePhysician;
+
+    @FindBy (xpath = "//*[text()='Save']")
+    public WebElement savebutonu;
+
+    @FindBy (xpath = "//div[@class='Toastify__toast Toastify__toast--success toastify-toast']")
+    public WebElement editToastContaınerText;
+
+    @FindBy (xpath = "(//*[text()='Delete'])[10]")
+    public WebElement deleteButonu;
+
+    @FindBy (xpath = "//*[text()='Are you sure you want to delete user admin?']")
+    public WebElement deletePopupText;
+
+    @FindBy (xpath = "/html/body/div[2]/div/div[1]/div/div/div[3]/button[2]")
+    public WebElement deletePopupDeleteButonu;
+
+    @FindBy (xpath = "//div[@class='Toastify__toast-body']")
+    public WebElement deleteToastContaınerText;
 
 
 
